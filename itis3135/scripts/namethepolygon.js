d83f9befe51e29a622e10626a092f08fb24e3d18
Tanim Pavel <tanimpavel7@gmail.com>
2021-10-03 14:54:13 -0400

    refactoring

--- itis3135/scripts/namethepolygon.js
+++ itis3135/scripts/namethepolygon.js
@@ -1,4 +1,4 @@
-const polygons = ["henagon","digon","trigon","tetragon","pentagon","hexagon","heptagon","octagon","enneagon","decagon"];
+const polygons = ["henagon","digon","trigon","tetragon","pentagon","hexagon","heptagon","octagon","enneagon","decagon","hendecagon"];
 
 function validateEntry(entry)
 {
@@ -7,7 +7,7 @@ function validateEntry(entry)
         return -1;
     }
     entry = Math.abs(Math.round(entry)); //Rounds and finds absolute value
-    if (entry>0 && entry<11)
+    if (entry>0 && entry<12)
     {
         return entry;
     }
@@ -25,10 +25,10 @@ function getShape(entry)
 
 function nameThePolygon()
 {
-    var userEntry = prompt("The Pavel Pelican would like you to enter a number between 0 and 10:");
+    var userEntry = prompt("The Pavel Pelican would like you to enter a number between 1 and 11:");
     while(validateEntry(userEntry) == -1)
     {
-        userEntry = prompt("We are sorry but the Pavel Pelican needs you to enter a number between 0 and 10");
+        userEntry = prompt("We are sorry but the Pavel Pelican needs you to enter a number between 1 and 11");
     }
     userEntry = validateEntry(userEntry); //Uses validateEntry to make sure entry is rounded and a positive value
     alert("Your polygon is a: " + getShape(userEntry));
