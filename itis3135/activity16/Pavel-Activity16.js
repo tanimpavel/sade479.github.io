98368ba0bdcff4b628d905a112a8d25b4d56b897
Tanim Pavel <tanimpavel7@gmail.com>
2021-11-28 12:45:12 -0500

    activity 15 and 16

---
+++ itis3135/activity16/Pavel-Activity16.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    const FLICKR_URL = "https://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
+    $.ajax({
+        type: "get",
+        url: FLICKR_URL,
+        beforeSend: function() {
+            $("#new_building").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function(data) {
+            $("#new_building").html("");
+            $.each(data.items, function(i, item) {
+                $("#new_building").append(
+                    "<a href='" + item.media.m + "' data-lightbox='new_building'" +
+                    " data-title='" + item.title + "' >" +
+                    "<img src='" + item.media.m + "'>" +
+                    "</a>"
+                );
+            });
+        }
+    });
+});
