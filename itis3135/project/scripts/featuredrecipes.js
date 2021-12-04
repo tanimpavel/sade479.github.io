05b329328e9fe14feafec002e18ad3b38054e97c
Tanim Pavel <tanimpavel7@gmail.com>
2021-12-04 12:30:59 -0500

    project activity 4 - add js

---
+++ itis3135/project/scripts/featuredrecipes.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.bxslider').bxSlider({
+      mode: 'fade',
+      auto: true,
+      pause: 6000,
+      touchEnabled: false,
+      slideWidth: 600
+    });
+  });
---
+++ itis3135/project/scripts/findingredients.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("#accordion").accordion({
+        heightStyle: "content",
+        collapsible: true
+    })
+});
